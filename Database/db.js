ded6676aacf2ee4649a7a7cdb18a252496f679f6
rahulrajamani31 <rahulrajamani310702@gmail.com>
2022-06-05 11:08:59 +0530

    aaaa

--- Database/db.js
+++ Database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB  = async () => {
     try {
-        const conn = await mongoose.connect("mongodb://localhost:27017/Farm_book?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false", {
+        const conn = await mongoose.connect("mongodb+srv://club_user:rmsr%4012345@cluster0.m3wnw.mongodb.net/farmbook?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
@@ -18,3 +18,4 @@ const connectDB  = async () => {
 module.exports=connectDB;
 
 //mongodb+srv://club_user:rmsr%4012345@cluster0.m3wnw.mongodb.net/farmbook?retryWrites=true&w=majority
+//"mongodb://localhost:27017/Farm_book?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
